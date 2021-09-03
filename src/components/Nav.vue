4a6e30dd9688bae63de1f98c9be7e0ef69848c50
tobiaspossetto <tobigpossetto@gmail.com>
2021-09-03 20:09:09 -0300

    Plantilla js

---
+++ src/components/Nav.vue
@@ -0,0 +1,52 @@
+<template>
+     <v-card
+    color="grey lighten-4"
+    flat
+    height="200px"
+    tile
+  >
+    <v-toolbar dense>
+      
+      <v-app-bar-nav-icon v-on:click = 'open()'></v-app-bar-nav-icon>
+
+      <v-toolbar-title>{{mensaje}}</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
+    </v-toolbar>
+  </v-card>
+</template>
+
+<script>
+    export default {
+         name: 'Home',
+
+ 
+  
+        data: () => ({
+           
+            mensaje:''
+        }),
+        methods: {
+             open: ()=>{
+               this.mensaje = 'asdasd'
+               console.log(this.mensaje)
+            }
+        }
+    }
+</script>
+
+<style lang="css" scoped>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,23 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+
+  <div><Nav/></div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
+ 
+ import Nav from '../components/Nav.vue'
+ export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+
+   components:{
+     Nav
+   },
+ 
+  
+    data: () => ({
+      
+    }),
+ 
+
+};
 </script>
